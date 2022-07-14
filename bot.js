ae3e58d400626bf363041b7fc90017fc4c26b6b9
ayoub-elkhadir <108630240+ayoub-elkhadir@users.noreply.github.com>
2022-07-14 22:50:35 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,15 +33,15 @@ global.APIKeys = {
 }
 
 //settings
-global.owner = ['27634090203',''] //owner number +27634090203
-global.wame = ['27634090203'] 
-global.pemilik = ['27634090203'] //owner number +27634090203
-global.premium = ['27634090203'] //premium number +27634090203
+global.owner = ['212617076646',''] //owner number +27634090203
+global.wame = ['212617076646'] 
+global.pemilik = ['212617076646'] //owner number +27634090203
+global.premium = ['212617076646'] //premium number +27634090203
 global.pengguna = 'Drips' //username
-global.footer = 'ZIM BOT INC'
+global.footer = 'ayoub elkhadir'
 global.botnma = 'ZIM BOT INC' //bot name
-global.botname = 'ZIM BOT V4'
-global.ownernma = 'Drips' //owner name
+global.botname = 'ayoub elkhadir'
+global.ownernma = 'ayoub' //owner name
 global.packname = 'Drips' //sticker package name
 global.author = 'By Drips Memes' //sticker author name
 global.sessionName = 'session' //session name
